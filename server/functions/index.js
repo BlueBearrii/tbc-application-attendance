8783b28bd52c22ecc057d5c094093b5c3aca5e5f
BlueBearrii <thananan.math.acs@gmail.com>
2021-02-15 14:13:00 +0700

    Cron job function

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,9 +1,57 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const db = admin.firestore();
+
+exports.scheduledFunction = functions.pubsub
+    .schedule("every day 22:00")
+    .timeZone("Asia/Bangkok")
+    .onRun((context) => {
+    // eslint-disable-next-line max-len
+      db.collection("approve")
+          .get()
+          .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+              doc.ref.update({
+                approveState: false,
+              });
+            });
+          })
+          .then(function() {
+            db.collection("check_in_switch")
+                .get()
+                .then(function(querySnapshot) {
+                  querySnapshot.forEach(function(doc) {
+                    doc.ref.update({
+                      checkInSwitch: false,
+                      checkInState: true,
+                    });
+                  });
+                });
+          });
+
+      return console.log("This will be run every minute!");
+    });
+
+
+exports.scheduledFunctionOpenSystem = functions.pubsub
+    .schedule("every day 7:00")
+    .timeZone("Asia/Bangkok")
+    .onRun((context) => {
+    // eslint-disable-next-line max-len
+      db.collection("check_in_switch")
+          .get()
+          .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+              doc.ref.update({
+                checkInSwitch: false,
+                checkInState: false,
+              });
+            });
+          });
+
+      return console.log("This will be run every minute!");
+    });
+
